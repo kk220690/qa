d95ee4a07b8f4f81440130b80e64b0e6ebad2470
krishna <krishna@krishna-PC>
2017-05-09 20:13:06 +0530

    automate

---
+++ qa/src/testcase/Tc_03.java
@@ -0,0 +1,21 @@
+package testcase;
+
+import org.testng.annotations.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+public class Tc_03 
+{
+	@Test
+	public void m1()
+	{
+		WebDriver d=new FirefoxDriver();
+		d.get("http://www.facebook.com");
+		d.findElement(By.id("email")).sendKeys("dckuhgjdc");
+		d.findElement(By.id("pass")).sendKeys("jgjsdc");
+		d.quit();
+	}
+
+}
